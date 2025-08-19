c655662778630ecf860a7d2e5e84042404ddb69a
JT <183244240+rtajt@users.noreply.github.com>
2025-08-19 13:27:00 -0400

    Create Tree.java

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+  public static void main(String[] args) {
+    // Just stating my name and printing trees
+    System.out.println("Hey there, I'm James and I'm learning to code in Java!");
+    System.out.println("I'm going to plant a tree today!");
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");
+  }
+}
