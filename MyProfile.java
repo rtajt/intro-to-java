4da079aaebae7319fe360fd2791ccd0800e75143
JT <183244240+rtajt@users.noreply.github.com>
2025-08-19 19:54:36 -0400

    Create MyProfile.java

---
+++ MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+  public static void main(String[] args) {
+    String name = "James";
+    int age = 17;
+    double desiredSalary = 91616.00;
+    char gender = 'm';
+    boolean lookingForJob = true;
+  }
+}
